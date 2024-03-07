d531007cd6f7adcfbe7636c5b86e6f74dfc6f071
yuuahmad-aii <ahmadyusuf@inovasi.ac.id>
2024-03-07 19:53:32 +0700

    rm pesan serial add led notif

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,15 @@
 FastAccelStepperEngine engine = FastAccelStepperEngine();
 FastAccelStepper *stepper = NULL;
 
-#define ledPin 13
+#define ledPin 13             // led notifikasi deccel state
+#define ledGerbangTerbuka 12  // led notif gerbang terbuka
+#define ledGerbangTertutup 11 // led notif gerbang tertutup
 #define limitPin A1
 #define receiverPin 2
 
 unsigned long debounceDuration = 1000; // millis
 unsigned long lastTimeButtonStateChanged = 0;
-int i = 0;
+// int i = 0;
 
 byte lastlimitState = LOW;
 byte lastReceiverState = LOW;
@@ -28,14 +30,14 @@ bool selesaiBukaTutup = true;
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println("START");
+  // Serial.begin(115200);
+  // Serial.println("START");
 
   engine.init();
   stepper = engine.stepperConnectToPin(stepPinStepper);
   if (stepper)
   {
-    Serial.println("HAVE STEPPER");
+    // Serial.println("HAVE STEPPER");
     stepper->setDirectionPin(dirPinStepper);
     stepper->setEnablePin(enablePinStepper);
     stepper->setAutoEnable(true);
@@ -45,27 +47,30 @@ void setup()
     // stepper->setDelayToDisable(1000);
 
     // speed up in ~0.025s, which needs 625 steps without linear mode
-    stepper->setSpeedInHz(2000);    // 500step/s
-    stepper->setAcceleration(1000); // 200step/s2
+    stepper->setSpeedInHz(2000);    // step/s
+    stepper->setAcceleration(8000); // step/s2
+    // stepper->set
   }
   else
   {
     while (true)
     {
-      Serial.println("NO STEPPER");
+      // Serial.println("NO STEPPER");
       delay(1000);
     }
   }
 
   pinMode(ledPin, OUTPUT);
+  pinMode(ledGerbangTerbuka, OUTPUT);
+  pinMode(ledGerbangTertutup, OUTPUT);
   pinMode(limitPin, INPUT_PULLUP);
   pinMode(receiverPin, INPUT_PULLUP);
 }
 
 void loop()
 {
+  // debounce sekaligus setting trigger jadi rising edge
   byte receiverState = digitalRead(receiverPin);
-
   if (millis() - lastTimeButtonStateChanged > debounceDuration)
   {
     byte limitState = digitalRead(limitPin);
@@ -78,9 +83,9 @@ void loop()
         decelState = true;
         // trigger = true;
         // do an action, for example print on Serial
-        Serial.print(i);
-        Serial.println("Button released");
-        i++;
+        // Serial.print(i);
+        // Serial.println("Button released");
+        // i++;
       }
     }
   }
@@ -101,17 +106,23 @@ void loop()
   }
 
   if (receiverState)
-    stepper->moveTo(10000);
+  {
+    stepper->moveTo(31168);
+    digitalWrite(ledPin, decelState);
+  }
   else if (!receiverState)
+  {
     stepper->moveTo(0);
+    digitalWrite(ledPin, decelState);
+  }
 
   if (decelState && !selesaiBukaTutup)
   {
     if (receiverState)
-      stepper->setCurrentPosition(8000);
+      stepper->setCurrentPosition(31168);
     else if (!receiverState)
-      stepper->setCurrentPosition(2000);
-    Serial.println("selesai!");
+      stepper->setCurrentPosition(0);
+    // Serial.println("selesai!");
     selesaiBukaTutup = true;
     decelState = false;
   }
